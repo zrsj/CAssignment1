27350f7e4845c6da1065a0151346917a81aa7520
zrsj <c3283006@uon.edu.au>
2019-04-11 14:14:01 +1000

    Modified main to accept command line args, header file created for functions.

---
+++ encdec.h
@@ -0,0 +1,51 @@
+/*Author: c3283006 (Zayne Jeffries)
+  Description: header file for encryption/decryption project
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//defines lookup table for substitution cipher based on QWERTY keyboard layout
+#define _A Q
+#define _B W
+#define _C E
+#define _D R
+#define _E T
+#define _F Y
+#define _G U
+#define _H I
+#define _I O
+#define _J P
+#define _K A
+#define _L S
+#define _M D
+#define _N F
+#define _O G
+#define _P H
+#define _Q J
+#define _R K
+#define _S L
+#define _T Z
+#define _U X
+#define _V C
+#define _W V
+#define _X B
+#define _Y N
+#define _Z M
+/*Function definitions*/
+char* rotencr(char message[], int key, size_t arrlen);
+void rotdecr(char ciph[], int key, size_t arrlen);
+char* subencr(char message[]);
+void subdecr(char ciph[]);
+void ciphrotdecr(char ciph[]);
+void ciphsubdecr(char ciph[]);
+void printerr();
+/*Function Implementations*/
+char* rotencr(char message[], int key, size_t arrlen){
+
+}
+
+void printerr(){
+	printf("USAGE: ./progname selection optional_key \"string_to_decrypt\"\n");
+	printf("Selection options:\n (1)Rotation Encrypt\n (2)Rotation Decrypt\n (3)Substitution Encrypt\n (4)Substitution Decrypt\n");
+	printf(" (5)Rotation Decrypt Cipher Text Only\n (6)Substitution Decrypt Cipher Text Only\n");
+}
--- main.c
+++ main.c
@@ -1,5 +1,38 @@
-#include <stdio.h>
+/*Author: c3283006 (Zayne Jeffries)
+  Description: main file for encryption/decryption assignment
+*/
+#include "encdec.h"
+#define MAX_ARGC 4
 
-int main(){
+int main(int argc, char* argv[]){
+	if(argc < 3){
+		printerr();
+		return -1;
+	}
+	int select = atoi(argv[1]);
+	char* text;
+	int key = 1;
+	if(argc == 4){
+		key = atoi(argv[2]);
+		text = strdup(argv[3]);
+	}
+	else
+		text = strdup(argv[2]);
+	switch(select){
+		case 1:
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+		default: printerr();
+		break;
+	}
 	return 0;
 }
