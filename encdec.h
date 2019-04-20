c1f1ab21825ecfc83749e4e83147266ad5e81016
zrsj <c3283006@uon.edu.au>
2019-04-20 10:39:24 +1000

    Implemented substitution encryption and decryption

--- encdec.h
+++ encdec.h
@@ -5,56 +5,204 @@
 #include <stdlib.h>
 #include <string.h>
 //defines lookup table for substitution cipher based on QWERTY keyboard layout
-#define _A Q
-#define _B W
-#define _C E
-#define _D R
-#define _E T
-#define _F Y
-#define _G U
-#define _H I
-#define _I O
-#define _J P
-#define _K A
-#define _L S
-#define _M D
-#define _N F
-#define _O G
-#define _P H
-#define _Q J
-#define _R K
-#define _S L
-#define _T Z
-#define _U X
-#define _V C
-#define _W V
-#define _X B
-#define _Y N
-#define _Z M
+#define _A 'Q'
+#define _B 'W'
+#define _C 'E'
+#define _D 'R'
+#define _E 'T'
+#define _F 'Y'
+#define _G 'U'
+#define _H 'I'
+#define _I 'O'
+#define _J 'P'
+#define _K 'A'
+#define _L 'S'
+#define _M 'D'
+#define _N 'F'
+#define _O 'G'
+#define _P 'H'
+#define _Q 'J'
+#define _R 'K'
+#define _S 'L'
+#define _T 'Z'
+#define _U 'X'
+#define _V 'C'
+#define _W 'V'
+#define _X 'B'
+#define _Y 'N'
+#define _Z 'M'
 /*Function definitions*/
-char* rotencr(char message[], char retarr[], int key, size_t arrlen);
-void rotdecr(char ciph[], int key, size_t arrlen);
-char* subencr(char message[]);
-void subdecr(char ciph[]);
-void ciphrotdecr(char ciph[]);
-void ciphsubdecr(char ciph[]);
+char* rotencr(char message[], int key, size_t arrlen);
+char* rotdecr(char ciph[], int key, size_t arrlen);
+char* subencr(char message[], size_t arrlen);
+char* subdecr(char ciph[], size_t arrlen);
+char* ciphrotdecr(char ciph[]);
+char* ciphsubdecr(char ciph[]);
 void printerr();
 /*Function Implementations*/
-char* rotencr(char message[], char retarr[], int key, size_t arrlen){
+char* rotencr(char message[], int key, size_t arrlen){
 	for(int i = 0; i < arrlen; i++){
 		if(message[i] == ' '){
-			retarr[i] = ' ';
+			message[i] = ' ';
 			continue;
 		}
-		retarr[i] = ((message[i] - 65) + key) % (26);
-		retarr[i] += 65;
+		message[i] = ((message[i] - 65) + key) % 26;
+		message[i] += 65;
 	}
-	retarr[arrlen - 1] = '\0';
-	return retarr;
+	message[arrlen] = '\0';
+	return message;
+}
+
+char* rotdecr(char ciph[], int key, size_t arrlen){
+	for(int i = 0; i < arrlen; i++){
+		if(ciph[i] == ' '){
+			ciph[i] = ' ';
+			continue;
+		}
+		ciph[i] = ((ciph[i] - 65) - key) % 26;
+		ciph[i] += 65;
+	}
+	ciph[arrlen] = '\0';
+	return ciph;
+}
+
+char* subencr(char message[], size_t arrlen){
+	for(int i = 0; i < arrlen; i++){
+		if(message[i] == ' '){
+			message[i] = ' ';
+			continue;
+		}
+		switch(message[i]){
+			case 'A': message[i] = _A;
+			break;
+			case 'B': message[i] = _B;
+			break;
+			case 'C': message[i] = _C;
+			break;
+			case 'D': message[i] = _D;
+			break;
+			case 'E': message[i] = _E;
+			break;
+			case 'F': message[i] = _F;
+			break;
+			case 'G': message[i] = _G;
+			break;
+			case 'H': message[i] = _H;
+			break;
+			case 'I': message[i] = _I;
+			break;
+			case 'J': message[i] = _J;
+			break;
+			case 'K': message[i] = _K;
+			break;
+			case 'L': message[i] = _L;
+			break;
+			case 'M': message[i] = _M;
+			break;
+			case 'N': message[i] = _N;
+			break;
+			case 'O': message[i] = _O;
+			break;
+			case 'P': message[i] = _P;
+			break;
+			case 'Q': message[i] = _Q;
+			break;
+			case 'R': message[i] = _R;
+			break;
+			case 'S': message[i] = _S;
+			break;
+			case 'T': message[i] = _T;
+			break;
+			case 'U': message[i] = _U;
+			break;
+			case 'V': message[i] = _V;
+			break;
+			case 'W': message[i] = _W;
+			break;
+			case 'X': message[i] = _X;
+			break;
+			case 'Y': message[i] = _Y;
+			break;
+			case 'Z': message[i] = _Z;
+			break;
+			default: message[i] = '\0';
+			break;
+		}
+	}
+	message[arrlen] = '\0';
+	return message;
+}
+
+char* subdecr(char ciph[], size_t arrlen){
+	for(int i = 0; i < arrlen; i++){
+		if(ciph[i] == ' '){
+			ciph[i] = ' ';
+			continue;
+		}
+		switch(ciph[i]){
+			case _A: ciph[i] = 'A';
+			break;
+			case _B: ciph[i] = 'B';
+			break;
+			case _C: ciph[i] = 'C';
+			break;
+			case _D: ciph[i] = 'D';
+			break;
+			case _E: ciph[i] = 'E';
+			break;
+			case _F: ciph[i] = 'F';
+			break;
+			case _G: ciph[i] = 'G';
+			break;
+			case _H: ciph[i] = 'H';
+			break;
+			case _I: ciph[i] = 'I';
+			break;
+			case _J: ciph[i] = 'J';
+			break;
+			case _K: ciph[i] = 'K';
+			break;
+			case _L: ciph[i] = 'L';
+			break;
+			case _M: ciph[i] = 'M';
+			break;
+			case _N: ciph[i] = 'N';
+			break;
+			case _O: ciph[i] = 'O';
+			break;
+			case _P: ciph[i] = 'P';
+			break;
+			case _Q: ciph[i] = 'Q';
+			break;
+			case _R: ciph[i] = 'R';
+			break;
+			case _S: ciph[i] = 'S';
+			break;
+			case _T: ciph[i] = 'T';
+			break;
+			case _U: ciph[i] = 'U';
+			break;
+			case _V: ciph[i] = 'V';
+			break;
+			case _W: ciph[i] = 'W';
+			break;
+			case _X: ciph[i] = 'X';
+			break;
+			case _Y: ciph[i] = 'Y';
+			break;
+			case _Z: ciph[i] = 'Z';
+			break;
+			default: ciph[i] = '\0';
+			break;
+		}
+	}
+	ciph[arrlen] = '\0';
+	return ciph;
 }
 
 void printerr(){
 	printf("USAGE: ./progname selection optional_key \"string_to_decrypt\"\n");
 	printf("Selection options:\n (1)Rotation Encrypt\n (2)Rotation Decrypt\n (3)Substitution Encrypt\n (4)Substitution Decrypt\n");
 	printf(" (5)Rotation Decrypt Cipher Text Only\n (6)Substitution Decrypt Cipher Text Only\n");
+	printf("-END ERROR MESSAGE-\n");
 }
--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main(int argc, char* argv[]){
 		printerr();
 		return -1;
 	}
-	int select = atoi(argv[1]);
+	unsigned int select = atoi(argv[1]);
+	unsigned int key = 1;
 	char* text;
-	int key = 1;
 	if(argc == 4){
 		key = atoi(argv[2]);
 		text = strdup(argv[3]);
@@ -20,15 +20,14 @@ int main(int argc, char* argv[]){
 		text = strdup(argv[2]);
 	}
 	size_t textlen = strlen(text);
-	char retarr[textlen];
 	switch(select){
-		case 1: printf("%s\n", rotencr(text, retarr, key, textlen));
+		case 1: printf("%s\n", rotencr(text, key, textlen));
 		break;
-		case 2:
+		case 2: printf("%s\n", rotdecr(text, key, textlen));
 		break;
-		case 3:
+		case 3: printf("%s\n", subencr(text, textlen));
 		break;
-		case 4:
+		case 4: printf("%s\n", subdecr(text, textlen));
 		break;
 		case 5:
 		break;
