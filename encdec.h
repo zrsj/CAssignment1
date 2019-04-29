4448f3f086bd8aa4b18af4cfb98897504676e4dc
zrsj <c3283006@uon.edu.au>
2019-04-29 16:34:01 +1000

    Now performs file I/O and correctly performs first 4 algorithms

--- encdec.h
+++ encdec.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 //defines lookup table for substitution cipher based on QWERTY keyboard layout
 #define _A 'Q'
 #define _B 'W'
@@ -32,21 +33,107 @@
 #define _Y 'N'
 #define _Z 'M'
 /*Function definitions*/
-char* rotencr(char message[], int key, size_t arrlen);
-char* rotdecr(char ciph[], int key, size_t arrlen);
-char* subencr(char message[], size_t arrlen);
-char* subdecr(char ciph[], size_t arrlen);
-char* ciphrotdecr(char ciph[]);
-char* ciphsubdecr(char ciph[]);
+/*maxfind():
+	INPUTS: integer array of tallies, integer array indicating used information, array length
+	RETURNS: integer (index of max)
+	DESCRIPTION: finds the location of the largest number in an array of integers, saves it once found in an array and returns the index
+	LIMITATIONS: none
+*/
+int maxfind(int search[], int used[], size_t arrlen);
+
+/*printerr():
+	INPUTS: none
+	RETURNS: void (none)
+	DESCRIPTION: prints an error message should input file not contain valid information
+	LIMITATIONS: none
+*/
 void printerr();
+/*charcheck():
+	INPUTS: single char (m)
+	RETURNS: integer, either 1 or 0
+	DESCRIPTION: returns 1 if char m is found to be a special character, 0 if it is a letter
+	LIMITATIONS: none
+*/
+int charcheck(char m);
+/*rotencr():
+	INPUTS: char array with message, integer specifying key, length of message
+	RETURNS: encrypted message array
+	DESCRIPTION: encrypts message contained within message string using key to specify rotational shift
+	LIMITATIONS: none
+*/
+char* rotencr(char message[], int key, size_t arrlen);
+/*rotdecr():
+	INPUTS: char array with ciphertext, integer specifying key, length of message
+	RETURNS: decrypted ciphertext array
+	DESCRIPTION: decrypts ciphertext contained within ciphertext string using key to specify rotational shift
+	LIMITATIONS: if encrypted with rotencr, the same key value must be used for this function
+*/
+char* rotdecr(char ciph[], int key, size_t arrlen);
+/*subencr():
+	INPUTS: char array with message, length of message
+	RETURNS: encrypted message array
+	DESCRIPTION: encrypts message contained within message string using a series of letter substitutions
+	LIMITATIONS: none
+*/
+char* subencr(char message[], size_t arrlen);
+/*subdecr():
+	INPUTS: char array with ciphertext, length of message
+	RETURNS: decrypted ciphertext array
+	DESCRIPTION: decrypts ciphertext containted within ciphertext string using a series of letter substitutions
+	LIMITATIONS: must have been encrypted by rotencr
+*/
+char* subdecr(char ciph[], size_t arrlen);
+/*ciphrotdecr():
+	INPUTS: char array with ciphertext, length of ciphertext
+	RETURNS: potentially decrypted ciphertext array
+	DESCRIPTION: attempts to decrypt rotational ciphertext encrypted with an unknown rotational key
+	LIMITATIONS: may not correctly decrypt entire message
+*/
+char* ciphrotdecr(char ciph[], size_t arrlen);
+/*ciphsubdecr():
+	INPUTS: char array with ciphertext, length of ciphertext
+	RETURNS: potentially decrypted ciphertext array
+	DESCRIPTION: attempts to decrypt substitutional ciphertext encrypted with an unknown substitution
+	LIMITATIONS: may not correctly decrypt entire message
+*/
+char* ciphsubdecr(char ciph[]);
 /*Function Implementations*/
+int maxfind(int search[], int used[], size_t arrlen){
+	int max = 0, max_ind = 0, i;
+	for(i = 0; i < arrlen; i++){
+		if(search[i] > max && used[i] != 1){
+			max = search[i];
+			max_ind = i;
+		}
+	}
+	used[max_ind] = 1;
+	return max_ind;
+}
+
+int charcheck(char m){
+	char specials[] = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '{', '}', '|', '~', '`', ':', ';', 
+		'<', '>', '?', ',', '.', '/', '\'', '\"', ' '};
+	for(int i = 0; i < strlen(specials); i++){
+		if(m == specials[i])
+			return 1;
+	}
+	return 0;
+}
+
+void printerr(){
+	printf("USAGE: ./progname input.txt\n");
+	printf("Selection options:\n (1)Rotation Encrypt\n (2)Rotation Decrypt\n (3)Substitution Encrypt\n (4)Substitution Decrypt\n");
+	printf(" (5)Rotation Decrypt Cipher Text Only\n (6)Substitution Decrypt Cipher Text Only\n");
+	printf("-END ERROR MESSAGE-\n");
+}
+
 char* rotencr(char message[], int key, size_t arrlen){
 	for(int i = 0; i < arrlen; i++){
-		if(message[i] == ' '){
-			message[i] = ' ';
+		if(charcheck(message[i])){
+			message[i] = message[i];
 			continue;
 		}
-		message[i] = ((message[i] - 65) + key) % 26;
+		message[i] = abs((message[i] - 65) + key) % 26;
 		message[i] += 65;
 	}
 	message[arrlen] = '\0';
@@ -55,11 +142,11 @@ char* rotencr(char message[], int key, size_t arrlen){
 
 char* rotdecr(char ciph[], int key, size_t arrlen){
 	for(int i = 0; i < arrlen; i++){
-		if(ciph[i] == ' '){
-			ciph[i] = ' ';
+		if(charcheck(ciph[i])){
+			ciph[i] = ciph[i];
 			continue;
 		}
-		ciph[i] = ((ciph[i] - 65) - key) % 26;
+		ciph[i] = abs((ciph[i] - 65) - key) % 26;
 		ciph[i] += 65;
 	}
 	ciph[arrlen] = '\0';
@@ -68,10 +155,8 @@ char* rotdecr(char ciph[], int key, size_t arrlen){
 
 char* subencr(char message[], size_t arrlen){
 	for(int i = 0; i < arrlen; i++){
-		if(message[i] == ' '){
-			message[i] = ' ';
+		if(message[i] == ' ')
 			continue;
-		}
 		switch(message[i]){
 			case 'A': message[i] = _A;
 			break;
@@ -135,10 +220,8 @@ char* subencr(char message[], size_t arrlen){
 
 char* subdecr(char ciph[], size_t arrlen){
 	for(int i = 0; i < arrlen; i++){
-		if(ciph[i] == ' '){
-			ciph[i] = ' ';
+		if(ciph[i] == ' ' || ciph[i] == '.' || ciph[i] == ',' || ciph[i] == ';')
 			continue;
-		}
 		switch(ciph[i]){
 			case _A: ciph[i] = 'A';
 			break;
@@ -200,9 +283,31 @@ char* subdecr(char ciph[], size_t arrlen){
 	return ciph;
 }
 
-void printerr(){
-	printf("USAGE: ./progname selection optional_key \"string_to_decrypt\"\n");
-	printf("Selection options:\n (1)Rotation Encrypt\n (2)Rotation Decrypt\n (3)Substitution Encrypt\n (4)Substitution Decrypt\n");
-	printf(" (5)Rotation Decrypt Cipher Text Only\n (6)Substitution Decrypt Cipher Text Only\n");
-	printf("-END ERROR MESSAGE-\n");
+char* ciphrotdecr(char ciph[], size_t arrlen){
+	//count letter freq
+	int lfreq[26] = {}; //holds frequency of letters counted from ciphertext, 26 size corresponding to all letters of the alphabet
+	int used[26] = {}; //holds 1 if used, 0 if not yet used corresponding to the letter
+	char letters[] = {'E', 'T', 'A', 'O', 'I', 'N', 'S', 'R', 'H', 'D', 'L', 'U', 'C', 'M', 'F', 'Y', 'W', 'G', 'P', 'B', 'V', 'K', 'X', 'Q', 'J', 'Z'};
+	int i, currentletter = 0, max = 0;
+	for(i = 0; i < arrlen; i++){
+		if(charcheck(ciph[i])){
+			continue;
+		}
+		lfreq[abs((ciph[i] - 65))]++;
+	}
+	//assign highest freq to corresponding english lang char
+	for(i = 0; i < 26; i++){
+		max = maxfind(lfreq, used, 26);
+		for(int j = 0; j < arrlen; j++){
+			if(abs(ciph[j] - 65) == max)
+				ciph[j] = letters[currentletter];
+		}
+		currentletter++;
+	}
+	ciph[arrlen] = '\0';
+	return ciph;
 }
+
+/*char* ciphsubdecr(){
+
+}*/
--- main.c
+++ main.c
@@ -2,39 +2,84 @@
   Description: main file for encryption/decryption assignment
 */
 #include "encdec.h"
-#define MAX_ARGC 4
-
+#define OUT "file.txt"
+#define MAX_BUFFER 1000
 int main(int argc, char* argv[]){
-	if(argc < 3){
-		printerr();
-		return -1;
+	FILE* fp; //pointer to file type, filename specified through command line
+	int sel = 0, key = 0, argsc = 0, charc = 0, iskey = 0; //any variable ending in c is a counter for their respective type
+	char* args[3]; //seperate from command line arguments, these hold the arguments for the algorithms
+	char* text; //used for storing message to be encrypted and writing it to output
+	char c; //holds the currently read character from the text file
+	for(int i = 0; i < 3; i++)
+		args[i] = malloc(MAX_BUFFER); //dynamically allocate memory so that char* variables can be appended to
+	fp = fopen(argv[1], "r");
+	while((c = getc(fp)) != EOF){
+		if(c == '#'){ //a key has been specified in the input file, set flag to true for key being present
+			iskey = 1; 
+			continue;	
+		}
+		if(c == ' ' && argsc < 2){ //indicates end of an argument reached and adds null escape character
+			argsc++;
+			args[argsc][charc++] = '\0';
+			charc = 0; //reset for next argument such that array of next argument can be filled from the beginning
+			continue;
+		}
+		if(c == '\n') continue; //skip all newlines in text file, input read in straight line
+		args[argsc][charc++] = c;
 	}
-	unsigned int select = atoi(argv[1]);
-	unsigned int key = 1;
-	char* text;
-	if(argc == 4){
-		key = atoi(argv[2]);
-		text = strdup(argv[3]);
-	}
-	else{
-		text = strdup(argv[2]);
-	}
+	charc = 0; //set back to 0 for later usage when reading output file
+	sel = atoi(args[0]); //argument 0 must always be a selection variable
+	key = (iskey)?atoi(args[1]):1; //iskey is a flag that determines whether or not a key has been passed to the program
+	strcat(args[1], " "); //if the key was not specified, the first word of the message will be stored in args[1]
+	text = (iskey)?strdup(args[2]):strcat(strdup(args[1]), strdup(args[2])); //if key is found, text is stored in args[2]. If not, concat args[1] and args[2]
+	fclose(fp);
 	size_t textlen = strlen(text);
-	switch(select){
-		case 1: printf("%s\n", rotencr(text, key, textlen));
-		break;
-		case 2: printf("%s\n", rotdecr(text, key, textlen));
-		break;
-		case 3: printf("%s\n", subencr(text, textlen));
-		break;
-		case 4: printf("%s\n", subdecr(text, textlen));
-		break;
-		case 5:
-		break;
+	switch(sel){
+		case 1:
+			fp = fopen(OUT, "w"); 
+			fprintf(fp, "%s\n", rotencr(text, key, textlen));
+			printf("%s\n", text);
+			break;
+		case 2:
+			fp = fopen(OUT, "r");
+			while((c = getc(fp)) != EOF){
+				text[charc++] = c;
+			}
+			fclose(fp);
+			fp = fopen(OUT, "w");
+			fprintf(fp, "%s\n", rotdecr(text, key, textlen));
+			printf("%s\n", text);
+			break;
+		case 3:
+			fp = fopen(OUT, "w");
+			fprintf(fp, "%s\n", subencr(text, textlen));
+			printf("%s\n", text);
+			break;
+		case 4: 
+			fp = fopen(OUT, "r");
+			while((c = getc(fp)) != EOF){
+				text[charc++] = c;
+			}
+			fclose(fp);
+			fp = fopen(OUT, "w");
+			fprintf(fp, "%s\n", subdecr(text, textlen));
+			printf("%s\n", text);
+			break;
+		case 5: 
+			fp = fopen(OUT, "w");
+			fprintf(fp, "%s\n", ciphrotdecr(text, textlen));
+			printf("%s\n", text);
+			break;
 		case 6:
-		break;
-		default: printerr();
-		break;
+			break;
+		default: 
+			printerr();
+			break;
+	}
+	fclose(fp);
+	for(int i = 0; i < 3; i++){
+		free(args[i]);
+		args[i] = NULL;
 	}
 	return 0;
 }
